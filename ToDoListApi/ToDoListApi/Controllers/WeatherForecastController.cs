5cb17e6764cf13381ce8d955f932b971c9750da1
Flamious <sokol_sokolov@list.ru>
2022-08-17 11:47:15 +0300

    Initialize project

---
+++ ToDoListApi/ToDoListApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
